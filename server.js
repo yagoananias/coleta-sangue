0ca8dcd0bf03f9b7ddfcd4e42b3db2b02a343378
Yago Ananias <yago_souza92@yahoo.com.br>
2020-04-29 17:35:24 -0300

    Integrando o Nunjucks

--- server.js
+++ server.js
@@ -1,8 +1,50 @@
-const express = require("express");
-const server = express();
+const express = require("express")
+const server = express()
+
+server.use(express.static('public'))
+
+server.use(express.urlencoded({extended:true}))
+
+const nunjucks = require("nunjucks")
+nunjucks.configure("./", { 
+  express: server,
+  noCache: true,
+})
+
+const donors = [
+  {
+    name: "Yago Lima",
+    blood: "O+"
+  },
+  {
+    name: "Andressa Lima",
+    blood: "A+"
+  },
+  {
+    name: "Bernardo Lima",
+    blood: "A+"
+  },
+  {
+    name: "Ana Rosa",
+    blood: "AB+"
+  },
+]
 
 server.get("/", function (require, response) {
-  return response.send("Ok, cheguei")
-});
+  return response.render("index.html", { donors })
+})
 
-server.listen(3000);
+server.post("/", function(require, response) {
+  const name = require.body.name
+  const email = require.body.email
+  const blood = require.body.blood
+
+  donors.push({
+    name: name,
+    blood: blood,
+  })
+
+  return response.redirect("/")
+})
+
+server.listen(3000)
